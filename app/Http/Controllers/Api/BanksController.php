22ecc1b180b6abdb980c26112579838506012aeb
Bruno Tomé <ibrunotome@gmail.com>
2017-01-17 00:49:25 -0200

    Exibindo um unico registro.

--- app/Http/Controllers/Api/BanksController.php
+++ app/Http/Controllers/Api/BanksController.php
@@ -49,16 +49,6 @@ class BanksController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -77,18 +67,8 @@ class BanksController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $result = \App\Bank::findOrFail($id);
+		return response()->json($result);
     }
 
     /**
